aa0136fd29657589e2c733df97f0c1cbd527f3ed
okawak <okawak@users.noreply.github.com>
2023-10-29 09:34:50 +0000

    deploy: f99dae906237b2ca53d40762737e1508847cdf88

--- index.search.js
+++ index.search.js
@@ -31,7 +31,7 @@ var relearn_search_index = [
   },
   {
     "breadcrumb": "Installation",
-    "content": "last modified: 2023-08-20 by Kodai Okawa The following machines have been tested for operation.\nAlmalinux 9.2 zsh 5.8 gcc 11.3.1 cmake 3.20.2 ROOT 6.28/04 yaml-cpp 0.7.0 Scripts for some automate operation are written in zsh format.\nAs for the Ubuntu 22.04 (LTS), there appeared to be a linking problem and a dummy file had to be set up to force the link.\n",
+    "content": "last modified: 2023-10-29 by Kodai Okawa The following machines have been tested for operation.\n1. Writer’s (Okawa’s) machine Almalinux 9.2 zsh 5.8 gcc 11.3.1 cmake 3.20.2 ROOT 6.28/04 yaml-cpp 0.7.0 2. CRIB’s analysis machine CentOS Linux release 7.9.2009 (Core) zsh 5.5.1 gcc 9.3.1 (using devtoolset-9) cmake 3.28 (install manually) ROOT 6.28/06 yaml-cpp 0.8.0 NOTE:\nFrom 2023/10, Ubuntu system is also avaliable. (issue48) Scripts for some automate operation are written in zsh format. ",
     "description": "",
     "tags": null,
     "title": "Requirements",
@@ -47,11 +47,11 @@ var relearn_search_index = [
   },
   {
     "breadcrumb": "Installation",
-    "content": "last modified: 2023-08-20 by Kodai Okawa Artemis uses ROOT library.\nWe confirmed that version 6.28 of the ROOT will work well, but if the ROOT updates, we are not sure that the ARTEMIS will work well, so we recommend to build from the sources.\nFor detailded information, please refer Installing ROOT Recommendations are described below.\n# The latest stable branch gets updated automatically on each release. # You may update your local copy by issuing a `git pull` command from within `root_src/`. cd hoge # where you want to install ROOT git clone --branch latest-stable --depth=1 https://github.com/root-project/root.git root_src mkdir root_build root_install \u0026\u0026 cd root_build cmake -DCMAKE_INSTALL_PREFIX=../root_install ../root_src # \u0026\u0026 check cmake configuration output for warnings or errors cmake --build . -- install -j4 # if you have 4 cores available for compilation source ../root_install/bin/thisroot.sh # or thisroot.{fish,csh}If there are any problems at the compile, additional packages may need to be installed. See also dependencies.\nsource thisroot.sh part will be written in thisartemis.sh (this is created after artemis installation), so you don’t have to execute this command for each time.\n",
+    "content": "last modified: 2023-10-29 by Kodai Okawa Generally, dnf install cmake or brew install cmake support latest version of the cmake, but for some older machine like CentOS 7, it is too old to install ROOT.\nThe latest ROOT require CMake 3.16 or higher, and if your system doesn’t support this version, you need to install manually.\nYou can get more information from here.\ngit clone https://github.com/Kitware/Cmake.git cd Cmake ./bootstrap make -j8 sudo make installIf you want to set the version, you can change the branch. The default branch is master.\n",
     "description": "",
-    "tags": null,
-    "title": "Root",
-    "uri": "/artemis_crib/installation/root/index.html"
+    "tags": [],
+    "title": "Cmake",
+    "uri": "/artemis_crib/installation/cmake/index.html"
   },
   {
     "breadcrumb": "",
@@ -63,11 +63,11 @@ var relearn_search_index = [
   },
   {
     "breadcrumb": "Installation",
-    "content": "last modified: 2023-08-20 by Kodai Okawa Current version of the artemis use yaml-cpp-0.7. If your package system (like apt or dnf) cannot install version 0.7, please install from the source.\nSee also Github repo and README of the artemis Please install it as a shared object like below commands.\ncd path/to/the/directory/you/want/to/install/yaml-cpp git clone https://github.com/jbeder/yaml-cpp.git cd yaml-cpp mkdir build \u0026\u0026 cd build cmake -DYAML_BUILD_SHARED_LIBS=ON -DBUILD_TESTING=OFF .. make sudo make installDefault install path is /usr/local/lib or /usr/local/lib64. If you do not want to pollute the general environment, specify CMAKE_INSTALL_PREFIX and set PATH environment variable.\ncmake -DCMAKE_INSTALL_PREFIX=/foo/bar -DYAML_BUILD_SHARED_LIBS=ON -DBUILD_TESTING=OFF ..",
+    "content": "last modified: 2023-10-29 by Kodai Okawa Artemis uses ROOT library.\nWe confirmed that version 6.28 of the ROOT will work well, but if the ROOT updates, we are not sure that the ARTEMIS will work well, so we recommend to build from the sources.\nFor detailded information, please refer Installing ROOT Recommendations are described below.\n# The latest stable branch gets updated automatically on each release. # You may update your local copy by issuing a `git pull` command from within `root_src/`. cd hoge # where you want to install ROOT git clone --branch latest-stable --depth=1 https://github.com/root-project/root.git root_src mkdir root_build root_install \u0026\u0026 cd root_build cmake -DCMAKE_INSTALL_PREFIX=../root_install ../root_src # \u0026\u0026 check cmake configuration output for warnings or errors cmake --build . -- install -j4 # if you have 4 cores available for compilation source ../root_install/bin/thisroot.sh # or thisroot.{fish,csh}If there are any problems at the compile, additional packages may need to be installed. See also dependencies.\nInfo If errors related to xrootd, TBB and clad occur, the cmake options -Dxrootd=OFF, -Dimt=OFF and -Dclad=OFF may work respectively.\nsource thisroot.sh part will be written in thisartemis.sh (this is created after artemis installation), but before installing artemis, you need to type this once.\nafter completing the installation of the artemis, you don’t have to execute this command for each time.\n",
     "description": "",
     "tags": null,
-    "title": "Yaml-cpp",
-    "uri": "/artemis_crib/installation/yaml-cpp/index.html"
+    "title": "Root",
+    "uri": "/artemis_crib/installation/root/index.html"
   },
   {
     "breadcrumb": "",
@@ -79,11 +79,11 @@ var relearn_search_index = [
   },
   {
     "breadcrumb": "Installation",
-    "content": "last modified: 2023-08-20 by Kodai Okawa From the current situation, CRIB experiment doesn’t use GET system, so we describe how to install it without linking it to GET decoder.\nAlso, it can link to openMPI, but the below commands assume not using openMPI. See artemis repo for more information.\ncd hoge git clone https://github.com/artemis-dev/artemis.git -b develop cd artemis mkdir build cd build cmake -DCMAKE_INSTALL_PREFIX=../install .. make -j8 make installThen, \u003cCMAKE_INSTALL_PREFIX\u003e/bin/thisartemis.sh will be created and this shell script can configure the environment (ROOT, yaml-cpp, artemis libraries) to use artemis.\n(It means you don’t have to write like export PATH=${yaml-cpp path}:$PATH or source thisroot.sh in .zshrc or .bashrc.)\nAnother option is to use module command to manage the environment. It is also written in artemis repo.\nWarning The following methods are not recommended!!\nFor Ubuntu case, somehow it seems that there are some linking problem.\nTo avoid it, if I changed like below, the artemis will work well…\n[Mod] artemis/sources/main/CMakeLists.txt - add_executable(artemis main.cc TArtRint.cc) + add_executable(artemis main.cc dummy.cc TArtRint.cc) [Add] artemis/sources/main/dummy.cc #include \u003cTModuleDecoderV1190.h\u003e #include \u003cTParameterLoader.h\u003e #include \u003cTTreeProjection.h\u003e #include \u003cTRDFEventStore.h\u003e #include \u003cTRIDFEventStore.h\u003e // any class seems okay void dummy() { art::TModuleDecoderV1190 *dummy_decoder = new art::TModuleDecoderV1190(); art::TParameterLoader *dummy_param = new art::TParameterLoader(); art::TTreeProjection *dummy_hist = new art::TTreeProjection(); art::TRDFEventStore *dummy_rdf = new art::TRDFEventStore(); art::TRIDFEventStore *dummy_ridf = new art::TRIDFEventStore(); }Libraries that are not directly involved in the artemis executable do not seem to be linked, despite using target_link_library(hoge) in CMakeLists.txt.\n",
+    "content": "last modified: 2023-10-29 by Kodai Okawa Current version of the artemis use yaml-cpp-0.7. If your package system (like apt or dnf) cannot install version 0.7, please install from the source.\nSee also Github repo and README of the artemis Please install it as a shared object like below commands.\ncd path/to/the/directory/you/want/to/install/yaml-cpp git clone https://github.com/jbeder/yaml-cpp.git cd yaml-cpp mkdir build \u0026\u0026 cd build cmake -DYAML_BUILD_SHARED_LIBS=ON -DBUILD_TESTING=OFF .. make sudo make installDefault install path is /usr/local/lib or /usr/local/lib64. If you do not want to pollute the general environment, specify CMAKE_INSTALL_PREFIX and set PATH environment variable.\ncmake -DCMAKE_INSTALL_PREFIX=/foo/bar -DYAML_BUILD_SHARED_LIBS=ON -DBUILD_TESTING=OFF ..",
     "description": "",
     "tags": null,
-    "title": "Artemis",
-    "uri": "/artemis_crib/installation/artemis/index.html"
+    "title": "Yaml-cpp",
+    "uri": "/artemis_crib/installation/yaml-cpp/index.html"
   },
   {
     "breadcrumb": "",
@@ -95,11 +95,13 @@ var relearn_search_index = [
   },
   {
     "breadcrumb": "Installation",
-    "content": "last modified: 2023-09-29 by Kodai Okawa For the convinience, we use one directory to store raw data (ridf files) and make symbolic link to each user work directory. So first, we need to make raw data directory.\nThere are three option to do so.\nput the files in the same storage put the files in the external storage use network file system (NFS) and mount it (like xfs) If you have large size of main storage, the one option is easiest way. Just like:\ncd ~ mkdir data (or where you want to put) cd data rsync hoge (cp or scp to put the raw data)The symbolic link process will be done in the next process.\nWhen your main storage is not so large, you may think to use external storage. For example, main storage is used for OS installation and external storage is used for experimental data. (I think this case is for personal analysis using your own PC.)\nIn that case, you need to do:\nmount the external storage check and set the file permission to be able to read or write it. The format and mount process is very depend on the situation, so please check the way in other place. One important point is that we have output root file when we start to analysis, so it may need to make the directory for outputed root files in the external storage.\nWhen you have file server, we can use network file system (NFS). This is CRIB online analysis case, so let me explain it in detail.\n--- title: Network system of CRIB --- graph LR; A(MPV E7) --\u003e D{\u003cstrong\u003eDAQ main PC\u003c/strong\u003e\u003cp\u003e\u003c/p\u003efile server} B(MPV1 J1) --\u003e D C(MPV2 J1) --\u003e D D --\u003e E[Analysis PC] As this figure, we use file server in the DAQ main PC.\nserver setting Actually now the PC is CentOS7, but for future setting, I will note the setting for newer OS. First, please install required package.\nsudo dnf install -y rpcbind nfs-utilsThen prepare the directory for share. Any name and place is okay.\nmkdir /share or /data And setting file for nfs is /etc/exports, and here is the example.\n/share/exp1 192.168.0.5/10(rw,no_root_squash,async) mkdir /share or /data cliant setting ",
+    "content": "last modified: 2023-10-29 by Kodai Okawa From the current situation, CRIB experiment doesn’t use GET system, so we describe how to install it without linking it to GET decoder.\nAlso, it can link to openMPI, but the below commands assume not using openMPI. See artemis repo for more information.\ncd hoge git clone https://github.com/artemis-dev/artemis.git -b develop cd artemis mkdir build cd build cmake -DCMAKE_INSTALL_PREFIX=../install .. make -j8 make install source ../install/bin/thisartemis.shThen, \u003cCMAKE_INSTALL_PREFIX\u003e/bin/thisartemis.sh will be created and this shell script can configure the environment (ROOT, yaml-cpp, artemis libraries) to use artemis.\n(It means you don’t have to write like export PATH=${yaml-cpp path}:$PATH or source thisroot.sh in .zshrc or .bashrc.)\nAnother option is to use module command to manage the environment. It is also written in artemis repo.\nNote This is 2023/10/29 current information\nOkawa think there is a grammatical error in the sources/main/thisartemis.sh.in, so I changed like:\n-if [ \"@BUILD_GET@\" == \"ON\" ]; then +if [[ \"@BUILD_GET@\" == \"ON\" ]]; then export LD_LIBRARY_PATH=@GET_LIB_DIR@:$LD_LIBRARY_PATH fi -if [ \"@MPI_CXX_FOUND@\" == \"TRUE\" ]; then +if [[ \"@MPI_CXX_FOUND@\" == \"TRUE\" ]]; then dir=@MPI_CXX_LIBRARIES@ libdir=\"$(dirname $dir)\" ",
     "description": "",
-    "tags": null,
-    "title": "Directory",
-    "uri": "/artemis_crib/installation/directory/index.html"
+    "tags": [
+      "CRIB"
+    ],
+    "title": "Artemis",
+    "uri": "/artemis_crib/installation/artemis/index.html"
   },
   {
     "breadcrumb": "",
@@ -111,11 +113,11 @@ var relearn_search_index = [
   },
   {
     "breadcrumb": "Installation",
-    "content": "last modified: 2023-08-20 by Kodai Okawa ",
+    "content": "last modified: 2023-10-29 by Kodai Okawa For the convinience, we use one directory to store raw data (ridf files) and make symbolic link to each user work directory. So first, we need to make raw data directory.\nThere are three option to do so.\nput the files in the same storage put the files in the external storage use network file system (NFS) and mount it (like xfs) If you have large size of main storage, the one option is easiest way. Just like:\ncd ~ mkdir data (or where you want to put) cd data rsync hoge (cp or scp to put the raw data)The symbolic link process will be done in the next process.\nWhen your main storage is not so large, you may think to use external storage. For example, main storage is used for OS installation and external storage is used for experimental data. (I think this case is for personal analysis using your own PC.)\nIn that case, you need to do:\nmount the external storage check and set the file permission to be able to read or write it. The format and mount process is very depend on the situation, so please check the way in other place. One important point is that we have output root file when we start to analysis, so it may need to make the directory for outputed root files in the external storage.\nWhen you have file server, we can use network file system (NFS). This is CRIB online analysis case, so let me explain it in detail.\n--- title: Network system of CRIB --- graph LR; A(MPV E7) --\u003e D{\u003cstrong\u003eDAQ main PC\u003c/strong\u003e\u003cp\u003e\u003c/p\u003efile server} B(MPV1 J1) --\u003e D C(MPV2 J1) --\u003e D D --\u003e E[Analysis PC] As this figure, we use file server in the DAQ main PC.\nserver setting Actually now the PC is CentOS7, but for future setting, I will note the setting for newer OS. First, please install required package.\nsudo dnf install -y rpcbind nfs-utilsThen prepare the directory for share. Any name and place is okay.\nmkdir /share or /data And setting file for nfs is /etc/exports, and here is the example.\n/share/exp1 192.168.0.5/10(rw,no_root_squash,async) mkdir /share or /data cliant setting ",
     "description": "",
     "tags": null,
-    "title": "Art_analysis",
-    "uri": "/artemis_crib/installation/art_analysis/index.html"
+    "title": "Directory",
+    "uri": "/artemis_crib/installation/directory/index.html"
   },
   {
     "breadcrumb": "",
@@ -132,6 +134,14 @@ var relearn_search_index = [
     "tags": null,
     "title": "Energyloss_calculator",
     "uri": "/artemis_crib/installation/energyloss_calculator/index.html"
+  },
+  {
+    "breadcrumb": "Installation",
+    "content": "last modified: 2023-10-29 by Kodai Okawa ",
+    "description": "",
+    "tags": null,
+    "title": "Art_analysis",
+    "uri": "/artemis_crib/installation/art_analysis/index.html"
   },
   {
     "breadcrumb": "Example",
@@ -220,6 +230,14 @@ var relearn_search_index = [
     "tags": null,
     "title": "Tags",
     "uri": "/artemis_crib/tags/index.html"
+  },
+  {
+    "breadcrumb": "Tags",
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tag :: CRIB",
+    "uri": "/artemis_crib/tags/crib/index.html"
   },
   {
     "breadcrumb": "",
