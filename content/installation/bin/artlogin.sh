702061ec90fa303ae28b6fdc674978eb9eb55ada
KodaiOkawa <okawa@cns.s.u-tokyo.ac.jp>
2023-11-12 14:12:51 +0900

    [scripts] use formatter

--- content/installation/bin/artlogin.sh
+++ content/installation/bin/artlogin.sh
@@ -1,69 +1,70 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ -z "$1" ]; then
     username=${ART_EXP_NAME}
 else
     username=$1
 fi
-    
+
 userdir=${ART_ANALYSIS_DIR}/${ART_EXP_NAME}/${username}
-    
-if [ ! -d ${ART_ANALYSIS_DIR}/${ART_EXP_NAME} ] ; then
-    printf "please make new ${ART_EXP_NAME} work directory by the \"artnew\" command\n"
+
+if [ ! -d "${ART_ANALYSIS_DIR}/${ART_EXP_NAME}" ]; then
+    printf "please make new %s work directory by the \"artnew\" command\n" "${ART_EXP_NAME}"
     return 1
 fi
 
-
-if [ -d ${userdir} ] ; then
-    cd ${userdir}
-    export ART_USER_FULLNAME="`git config user.name`"
-    export ART_USER_EMAIL="`git config user.email`"
-    export ARTEMIS_WORKDIR=${userdir}
-    export ARTEMIS_USER=${username}
+if [ -d "${userdir}" ]; then
+    cd "${userdir}" || return 1
+    ART_USER_FULLNAME="$(git config user.name)"
+    export ART_USER_FULLNAME
+    ART_USER_EMAIL="$(git config user.email)"
+    export ART_USER_EMAIL
+    export ARTEMIS_WORKDIR="${userdir}"
+    export ARTEMIS_USER="${username}"
     alias acd='cd ${ARTEMIS_WORKDIR}'
 
-    if [ -e ${ARTEMIS_WORKDIR}/thisartemis-crib.sh ] ; then
-      source ${ARTEMIS_WORKDIR}/thisartemis-crib.sh
+    if [ -e "${ARTEMIS_WORKDIR}/thisartemis-crib.sh" ]; then
+        source "${ARTEMIS_WORKDIR}/thisartemis-crib.sh"
     fi
 
     return 0
-elif [ -e ${userdir} ] ; then
-    printf "\33[1martlogin\33[0m: strange file ${userdir} exist.\n"
+elif [ -e "${userdir}" ]; then
+    printf "\33[1martlogin\33[0m: strange file %s exist.\n" "${userdir}"
     return 1
 fi
 
-printf "\33[1martlogin\33[0m: user '$username' not found.\n"
+printf "\33[1martlogin\33[0m: user '%s' not found.\n" "${username}"
 while true; do
     printf "create new user? (y/n): "
-    read _answer
+    read -r _answer
     case ${_answer} in
-        y)
-            break
-            ;;
-        n)
-            echo "cancelled."
-            return 0
-            ;;
+    y)
+        break
+        ;;
+    n)
+        echo "cancelled."
+        return 0
+        ;;
     esac
 done
 
 export ARTEMIS_WORKDIR=${userdir}
 export ARTEMIS_USER=${username}
-git clone ${ART_USER_REPOS} ${userdir}
+git clone "${ART_USER_REPOS}" "${userdir}"
 
-cd ${userdir}
+cd "${userdir}" || return 1
 alias acd='cd ${ARTEMIS_WORKDIR}'
 
 printf "\33[1martlogin\33[0m: making local git config\n"
 while true; do
     printf "Input fullname: "
-    read _fullname
+    read -r _fullname
     printf "Is it Okay? (y/n): "
-    read _answer
+    read -r _answer
     case ${_answer} in
-        y)
-            break
-            ;;
+    y)
+        break
+        ;;
     esac
 done
 
@@ -71,32 +72,31 @@ git config user.name "${_fullname}"
 
 while true; do
     printf "Input email address: "
-    read _email
+    read -r _email
     printf "Is it Okay? (y/n): "
-    read _answer
+    read -r _answer
     case ${_answer} in
-        y)
-            break
-            ;;
+    y)
+        break
+        ;;
     esac
 done
 
 git config user.email "${_email}"
 
-
-if [ -d ${ART_DATA_DIR} ]; then
-    ln -fs ${ART_DATA_DIR} ${ARTEMIS_WORKDIR}/ridf
+if [ -d "${ART_DATA_DIR}" ]; then
+    ln -fs "${ART_DATA_DIR}" "${ARTEMIS_WORKDIR}/ridf"
 else
-    printf "\33[1martlogin\33[0m: ${ART_DATA_DIR} is not existed.\n"
+    printf "\33[1martlogin\33[0m: %s is not existed.\n" "${ART_DATA_DIR}"
 fi
 
-if [ ! -d ${OUTPUT_PATH}/${username}/output ] ; then
-    mkdir -p ${OUTPUT_PATH}/${username}/output
+if [ ! -d "${OUTPUT_PATH}/${username}/output" ]; then
+    mkdir -p "${OUTPUT_PATH}/${username}/output"
 fi
 
-if [ ! -d ${OUTPUT_PATH}/${username}/rootfile ] ; then
-    mkdir -p ${OUTPUT_PATH}/${username}/rootfile
+if [ ! -d "${OUTPUT_PATH}/${username}/rootfile" ]; then
+    mkdir -p "${OUTPUT_PATH}/${username}/rootfile"
 fi
 
-ln -fs ${OUTPUT_PATH}/${username}/output ${ARTEMIS_WORKDIR}/output
-ln -fs ${OUTPUT_PATH}/${username}/rootfile ${ARTEMIS_WORKDIR}/rootfile
+ln -fs "${OUTPUT_PATH}/${username}/output" "${ARTEMIS_WORKDIR}/output"
+ln -fs "${OUTPUT_PATH}/${username}/rootfile" "${ARTEMIS_WORKDIR}/rootfile"
