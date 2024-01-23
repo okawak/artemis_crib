b4c5ba241b292290f245ef16a1ff3cf5215aba36
okawak <kodak2022@outlook.jp>
2024-01-23 19:06:29 +0900

    add: SRIM dE-E plot

--- userlogon.C
+++ userlogon.C
@@ -2,6 +2,7 @@
     // load user function
     gROOT->ProcessLine(".L macro/load_tcutg.C");
     gROOT->ProcessLine(".L macro/kinematics.C");
+    gROOT->ProcessLine(".L macro/dE_E_plot.C");
     gROOT->ProcessLine(".L macro/scaler.C");
 
     // User commands register
